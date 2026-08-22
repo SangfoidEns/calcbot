0301c9f29b54a3605a9811611fcea1e5553ba4f8
SangfoidEns <90978826+SangfoidEns@users.noreply.github.com>
2026-08-22 12:30:42 +0200

    Refactor getTelegramUser function for user data retrieval

--- js/telegram.js
+++ js/telegram.js
@@ -1,20 +1,28 @@
 export function getTelegramUser() {
-  if (window.Telegram && window.Telegram.WebApp && window.Telegram.WebApp.initDataUnsafe?.user) {
-    const tgUser = window.Telegram.WebApp.initDataUnsafe.user;
+  const tg = window.Telegram?.WebApp;
+
+  if (tg) {
+    tg.ready();
+    tg.expand();
+  }
+
+  const user = tg?.initDataUnsafe?.user;
+
+  if (user) {
     return {
-      id: tgUser.id.toString(),
-      firstName: tgUser.first_name || 'User',
-      lastName: tgUser.last_name || '',
-      username: tgUser.username ? `@${tgUser.username}` : '',
-      photoUrl: tgUser.photo_url || ''
+      id: user.id,
+      firstName: user.first_name || 'User',
+      lastName: user.last_name || '',
+      username: user.username ? `@${user.username}` : '',
+      photoUrl: user.photo_url || null
     };
   }
 
   return {
-    id: 'local_dev_user',
-    firstName: 'Dev',
-    lastName: 'Admin',
-    username: '@local_dev',
-    photoUrl: ''
+    id: 'local_dev_id',
+    firstName: 'Демо',
+    lastName: 'Користувач',
+    username: '@demo_user',
+    photoUrl: null
   };
 }
